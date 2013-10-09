442243b4d55e62d31aef8ea195b2e2ea556bc44e
asassu <andrea.sassu@colorado.edu>
2013-10-09 10:19:15 -0700

    Savior

---
+++ db/app/helpers/movies_helper.rb
@@ -0,0 +1,13 @@
+module MoviesHelper
+  # Checks if a number is odd:
+  def oddness(count)
+    count.odd? ?  "odd" :  "even"
+  end
+  
+  def createParamsArray(sortColumn)
+    @paramsArray = Hash.new
+    @paramsArray.merge!({:ratings => @checked_ratings_hash})
+    @paramsArray.merge!({:sort_by => sortColumn})
+    @paramsArray
+  end
+end
